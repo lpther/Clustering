45549ba7c52271bc79851691df30065934d4191f
Louis-Philippe Theriault <lpther@gmail.com>
2013-05-10 01:19:27 +0000

    * display symmetric files implementation * display symmetric commands implementation

--- clustering-plugin/clustering.py
+++ clustering-plugin/clustering.py
@@ -1,7 +1,26 @@
+'''Clustering plugin (designed to sysadmin-toolkit)
+
+The clustering plugin provides a few commands to ensure important files
+and command output in the cluster are symmetric.
+
+This plugin provides a clustering service for other plugins.
+
+Run "debug commandprompt" for a list of available commands for this plugin,
+or "debug clustering" for a list of configured elements.
+
+Main website:
+    https://github.com/lpther/Clustering
+
+'''
+
+__version__ = '0.1.0'
+
 import ClusterShell
 import ClusterShell.Task
 import sysadmintoolkit
 import logging
+
+ALLOWED_SYMMETRIC_FILES_DIRECTIVES = ['recursive']
 
 global plugin_instance
 
@@ -42,10 +61,19 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
         self.start_task()
 
         self.add_command(sysadmintoolkit.command.ExecCommand('debug clustering', self, self.debug))
+        self.add_command(sysadmintoolkit.command.ExecCommand('show cluster symmetric-files', self, self.display_symmetric_files))
+        self.add_command(sysadmintoolkit.command.ExecCommand('show cluster symmetric-files group <nodeset>', self, self.display_symmetric_files))
+        self.add_command(sysadmintoolkit.command.ExecCommand('show cluster symmetric-commands', self, self.display_symmetric_commands))
+        self.add_command(sysadmintoolkit.command.ExecCommand('show cluster symmetric-commands group <nodeset>', self, self.display_symmetric_commands))
+
+        self.add_dynamic_keyword_fn('<nodeset>', self.get_nodesets)
 
         self.logger.debug('Clustering plugin started with default nodes: %s' % self.nodesets['default'])
 
     def start_task(self):
+        '''
+        Starts the clustershell task for file copy or shell commands dispatch to the cluster
+        '''
         self.clustershell_task = ClusterShell.Task.task_self()
         self.clustershell_task.set_info('connect_timeout', self.task_connect_timeout)
         self.clustershell_task.set_info('fanout', 512)
@@ -59,16 +87,33 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
             self.clustershell_task.set_info('print_debug', log_debug)
 
     def register_symmetric_files(self):
+        '''
+        Loads the symmetric files from the config argument at instantiation
+        '''
         for entry in self.config['symmetric-files'].split(','):
             if entry is '':
                 continue
 
             if ':' in entry:
                 group = entry.strip().split(':')[0]
-                file = ':'.join(entry.strip().split(':')[1:])
+                sym_file = ':'.join(entry.strip().split(':')[1:])
             else:
                 group = 'default'
-                file = entry.strip()
+                sym_file = entry.strip()
+
+            directives = []
+            if '|' in sym_file:
+                directives_dict = {}
+                for directive in sym_file.split('|')[0].split(','):
+                    if directive in ALLOWED_SYMMETRIC_FILES_DIRECTIVES:
+                        directives_dict[directive] = None
+                    else:
+                        self.logger.warning('Invalid directive in entry %s' % entry.strip())
+
+                directives = directives_dict.keys()
+                directives.sort()
+
+                sym_file = sym_file.split('|')[1]
 
             if group is '' or '/' in group or not group.startswith('@'):
                 group = 'default'
@@ -80,14 +125,16 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
                         continue
 
             if group not in self.symmetric_files:
-                self.symmetric_files[group] = []
+                self.symmetric_files[group] = {}
 
-            self.logger.debug('Adding symmetric file "%s" to group %s' % (file, group))
+            self.logger.debug('Adding symmetric file "%s" to group %s' % (sym_file, group))
 
-            self.symmetric_files[group].append(file)
-            self.symmetric_files[group].sort()
+            self.symmetric_files[group][sym_file] = directives
 
     def register_symmetric_commands(self):
+        '''
+        Loads the symmetric commands from the config argument at instantiation
+        '''
         for entry in self.config['symmetric-commands'].split(','):
             if entry is '':
                 continue
@@ -119,6 +166,9 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
             self.symmetric_commands[group].sort()
 
     def register_nodes(self, setname, nodes):
+        '''
+        Used by other plugins to register nodesets
+        '''
         if setname in self.nodesets:
             return
 
@@ -135,7 +185,10 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
 
 
     def test_reachability(self, nodeset='default'):
-        worker = self.clustershell_task.shell('echo CONNECTED', nodes=self.nodesets[nodeset])
+        '''
+        Simple test to ensure nodes are reachable
+        '''
+        self.clustershell_task.shell('echo CONNECTED', nodes=self.nodesets[nodeset])
         self.clustershell_task.resume()
 
         reachable_nodes = []
@@ -149,12 +202,104 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
 
     def leave_mode(self, cmdprompt):
         '''
+        Kills the clustershell task before quitting the last command prompt
         '''
         if cmdprompt is self.cmdstack[0]:
             self.logger.debug('Killing ClusterShell task')
             self.clustershell_task.abort(kill=True)
 
         super(Clustering, self).leave_mode(cmdprompt)
+
+    # Dynamic keywords
+
+    def get_nodesets(self, dyn_keyword=None):
+        '''
+        Returns the list of registered nodesets and description
+        '''
+        nodesets = self.nodesets.keys()
+        nodesets.sort()
+
+        nodesetmap = {}
+
+        for nodeset in nodesets:
+            nodesetmap[nodeset] = '%s nodeset' % nodeset
+
+        return nodesetmap
+
+    def display_symmetric_buffers(self, buffers):
+        '''
+        Displays the content of a shell task's buffers, and displays symmetric/asymmetric information
+        '''
+        for i in range(len(buffers)):
+            if i is 0:
+                if len(buffers) is 1:
+                    print sysadmintoolkit.utils.get_green_text('Symmteric')
+                else:
+                    print sysadmintoolkit.utils.get_red_text('Asymmteric')
+
+            print '      %s:' % ', '.join(buffers[i][1])
+            print sysadmintoolkit.utils.indent_text(buffers[i][0], indent=8, width=self.cmdstack[-1].width)
+
+    # Sysadmin-toolkit commands
+
+    def display_symmetric_files(self, line, mode):
+        '''
+        Displays symmetric files for the specified group
+        '''
+        if 'group' in line:
+            group = [line.split()[-1]]
+        else:
+            group = self.get_nodesets().keys()
+            group.sort()
+
+        self.logger.debug('Displaying symmetric files for groups: %s' % group)
+
+        for nodeset in group:
+            print 'Group: %s' % nodeset
+            print
+
+            sym_file_keys = self.symmetric_files[nodeset].keys()
+            sym_file_keys.sort()
+
+            for sym_file in sym_file_keys:
+                if 'recursive' in self.symmetric_files[nodeset][sym_file]:
+                    self.clustershell_task.shell('find %s -type f | xargs md5sum' % sym_file, nodes=self.nodesets[nodeset])
+                else:
+                    self.clustershell_task.shell('md5sum %s' % sym_file, nodes=self.nodesets[nodeset])
+
+                if len(self.symmetric_files[nodeset][sym_file]):
+                    print '  %s (%s):' % (sym_file, ','.join(self.symmetric_files[nodeset][sym_file])),
+                else:
+                    print '  %s:' % sym_file,
+
+                self.clustershell_task.resume()
+
+                self.display_symmetric_buffers([bufr_nodes for bufr_nodes in self.clustershell_task.iter_buffers()])
+
+    def display_symmetric_commands(self, line, mode):
+        '''
+        Displays symmetric commands for the specified group
+        '''
+        if 'group' in line:
+            group = [line.split()[-1]]
+        else:
+            group = self.get_nodesets().keys()
+            group.sort()
+
+        self.logger.debug('Displaying symmetric commands for groups: %s' % group)
+
+        for nodeset in group:
+            print 'Group: %s' % nodeset
+            print
+
+            for sym_command in self.symmetric_commands[nodeset]:
+                self.clustershell_task.shell(sym_command, nodes=self.nodesets[nodeset])
+
+                print '  "%s" :' % sym_command,
+
+                self.clustershell_task.resume()
+
+                self.display_symmetric_buffers([bufr_nodes for bufr_nodes in self.clustershell_task.iter_buffers()])
 
     def debug(self, line, mode):
         '''
@@ -166,7 +311,8 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
 
         print 'Clustering plugin configuration and state:'
         print
-        print '  ClusterShell Version: %s' % ClusterShell.__version__
+        print '  Clustering plugin version: %s' % __version__
+        print '  ClusterShell version: %s' % ClusterShell.__version__
         print
 
         registered_nodes = self.nodesets.keys()
@@ -174,33 +320,42 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
         registered_nodes.sort()
         registered_nodes.insert(0, 'default')
 
-        for nodeset in registered_nodes:
-            print '  Node group %s: %s - Reachable Nodes: %s' % (nodeset, str(self.nodesets[nodeset]).replace(',', ', '), str(self.reachable_nodes[nodeset]).replace(',', ', '))
-
+        print '  *** Registered node groups ***'
         print
+
+        for nodeset in registered_nodes:
+            print '    %s: %s' % (nodeset, str(self.nodesets[nodeset]).replace(',', ', '))
+            print '    reachable nodes: %s' % (str(self.reachable_nodes[nodeset]).replace(',', ', '))
+            print
 
         if len(self.symmetric_files):
             groups = self.symmetric_files.keys()
             groups.sort()
 
-            print '  Symmetric files configuration:'
+            print '  *** Symmetric files configuration ***'
             print
 
             for group in groups:
                 print '    Group: %s' % group
 
-                for file in self.symmetric_files[group]:
-                    print '      %s' % file
+                files = self.symmetric_files[group].keys()
+                files.sort()
+
+                for file in files:
+                    print '      %s' % file,
+
+                    if len(self.symmetric_files[group][file]) > 0:
+                        print '(%s)' % ','.join(self.symmetric_files[group][file])
+                    else:
+                        print
 
                 print
-
-            print
 
         if len(self.symmetric_commands):
             groups = self.symmetric_commands.keys()
             groups.sort()
 
-            print '  Symmetric commands configuration:'
+            print '  *** Symmetric commands configuration ***'
             print
 
             for group in groups:
@@ -210,6 +365,3 @@ class Clustering(sysadmintoolkit.plugin.Plugin):
                     print '      %s' % command
 
                 print
-
-            print
-
